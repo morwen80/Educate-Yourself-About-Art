2673e119b8edb27b96893784df27e03c9866e8fe
Claudia Fox <claudiafox95@icloud.com>
2019-01-03 15:53:57 +0000

    Edited Artworks Controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,16 @@
 class ArtworksController < ApplicationController
+  before_action :artist
+
   def index
+    @artworks = Artwork.all
   end
 
   def show
+    @artwork = Artwork.find(params[:id])
+  end
+  
+  private
+  def artist
+    @artist = Artist.find(params[:artist_id])
   end
 end
